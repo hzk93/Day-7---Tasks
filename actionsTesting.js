a9b57470ff6ab3aa817e51a332981b4f7a8c7f84
Hassan Zahoor <HassanZahoor@corp.contoursoftware.net>
2022-11-08 18:09:44 +0500

    Updated

--- actionsTesting.js
+++ actionsTesting.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
     $('#animateP').animate({color:"blue", opacity:"0.50"}, 6000,function(){$('#animateCompleted').append('Animation Completed !!!')});
+    
     $('#stopAnimation').click(function(){
         $('#animateP').stop();
     })
+    
     $('#usefadeOut').fadeOut(5000,function(){
-        $("#fadeOutCompleted").append('.fadeOut Action Completed')
+        $("#fadeOutCompleted").append('.fadeOut() Action Completed')
+    })
+
+    $('#usingFadeTo').fadeTo(6000, 0.4, function(){
+        $('#fadeToCompleted').append('.fadeTo() Action Completed')
     })
 })
